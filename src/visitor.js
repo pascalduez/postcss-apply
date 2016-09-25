7f0019769aab8709513f1b52af978d7780616b51
Pascal Duez <public@pascalduez.me>
2016-09-25 15:26:48 +0200

    Implement a new `preserve` option

--- src/visitor.js
+++ src/visitor.js
@@ -1,4 +1,5 @@
 import balanced from 'balanced-match';
+// import postcss from 'postcss';
 
 const RE_PROP_SET = /^(--)([\w-]+)(\s*)([:]?)$/;
 
@@ -7,6 +8,14 @@ export default class Visitor {
 
   cache = {};
   result = {};
+  defaults = { preserve: false };
+
+  constructor(options) {
+    this.options = {
+      ...this.defaults,
+      ...options,
+    };
+  }
 
   /**
    * Collect all `:root` declared property sets and save them.
@@ -35,9 +44,12 @@ export default class Visitor {
     // Custom property sets override each other wholly,
     // rather than cascading together like colliding style rules do.
     // @see: https://tabatkins.github.io/specs/css-apply-rule/#defining
-    this.cache[setName] = rule;
+    const newRule = rule.clone();
+    this.cache[setName] = newRule;
 
-    rule.remove();
+    if (!this.options.preserve) {
+      rule.remove();
+    }
 
     if (!parent.nodes.length) {
       parent.remove();
@@ -48,9 +60,12 @@ export default class Visitor {
    * Replace nested `@apply` at-rules declarations.
    */
   resolveNested = () => {
-    Object.keys(this.cache).forEach(rule =>
-      this.cache[rule].walkAtRules('apply', this.resolve)
-    );
+    Object.keys(this.cache).forEach((rule) => {
+      this.cache[rule].walkAtRules('apply', (atRule) => {
+        this.resolve(atRule);
+        atRule.remove();
+      });
+    });
   }
 
   /**
@@ -58,6 +73,19 @@ export default class Visitor {
    * @param {Node} atRule
    */
   resolve = (atRule) => {
+    if (atRule.parent.type !== 'rule') {
+      atRule.warn(this.result,
+        'The @apply rule can only be declared inside Rule type nodes.'
+      );
+
+      atRule.remove();
+      return;
+    }
+
+    if (isDefinition(atRule.parent)) {
+      return;
+    }
+
     const param = getParamValue(atRule.params);
     const matches = RE_PROP_SET.exec(param);
 
@@ -66,15 +94,35 @@ export default class Visitor {
     }
 
     const setName = matches[2];
+    const parent = atRule.parent;
 
-    if (setName in this.cache) {
-      atRule.replaceWith(this.cache[setName].nodes);
-    } else {
+    if (!(setName in this.cache)) {
       atRule.warn(this.result,
         `No custom property set declared for \`${setName}\`.`
       );
+
+      return;
     }
+
+    if (this.options.preserve) {
+      parent.insertBefore(atRule, this.cache[setName].nodes);
+
+      return;
+    }
+
+    atRule.replaceWith(this.cache[setName].nodes);
   }
+}
+
+
+/**
+ * Helper: return whether the rule is a custom property set definition.
+ * @param {Object} rule
+ * @return {Boolean}
+ */
+function isDefinition(rule) {
+  return (rule.selector && RE_PROP_SET.exec(rule.selector))
+    && (rule.parent && rule.parent.selector && rule.parent.selector === ':root');
 }
 
 
---
+++ test/preserve.test.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import plugin from '../src';
+
+const read = name =>
+  fs.readFileSync(path.join(__dirname, 'fixture', name), 'utf8');
+
+
+describe('the `preserve` option', () => {
+  it('should properly apply and preserve custom property sets', async () => {
+    const input = read('preserve/input.css');
+    const expected = read('preserve/expected.css');
+
+    const result = await postcss()
+      .use(plugin({ preserve: true }))
+      .process(input);
+
+    expect(result.css).toBe(expected);
+  });
+});
